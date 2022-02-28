ed8f7f3b8a1e3cb1cd949d74eef52732f4214078
mameMan <jokingbear01@gmail.com>
2022-02-28 10:17:11 +0900

    section 25~26. tree, tree_width_with_level_width

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,54 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    const node = new Node(data);
+    this.children.push(node);
+  }
+
+  remove(data) {
+    this.children = this.children.filter((node) => {
+      return node.data !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      // 그냥 node.children을 push하면 array로 들어가서 nested array 되기 때문에 하나씩 꺼내어 push
+      arr.push(...node.children);
+      //   for (let child of node.children) {
+      //     arr.push(child);
+      //   }
+      fn(node);
+    }
+  }
+
+  traverseDF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
+}
+
+const node = new Node(1);
+const tree = new Tree();
 
 module.exports = { Tree, Node };
