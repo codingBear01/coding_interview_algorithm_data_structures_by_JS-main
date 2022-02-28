093cddd1e64e93f60f5eb8f9eaaa685c46296a67
codingBear <95178119+KiRinG01@users.noreply.github.com>
2022-03-01 07:47:08 +0900

    section 32. sort_by_selection

---
+++ exercises/sorting/app.js
@@ -0,0 +1,16 @@
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; ++i) {
+//     for (let j = 0; j < arr.length - 1 - i; ++j) {
+//       if (arr[j] > arr[j + 1]) {
+//         const lesser = arr[j + 1];
+//         arr[j + 1] = arr[j];
+//         arr[j] = lesser;
+//       }
+//     }
+//   }
+
+//   return arr;
+// }
+
+// const input = [5, 4, 3, 2, 1];
+// console.log(bubbleSort(input));
